970e2548e8a83fb5ea3aa54f6cd9a70a07cbf736
Jhonatanmizu <natanjesuss20@gmail.com>
2023-09-28 00:05:03 -0300

    :sparkles: created players controller

---
+++ src/players/players.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+// DTOS
+import { CreatePlayerDto } from './dtos/create-player.dto';
+// Services
+import { PlayersService } from './players.service';
+// Types
+import { Players } from './players';
+
+@Controller('api/v1/players')
+export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+  @Post()
+  async createOrUpdatePlayer(@Body() player: CreatePlayerDto) {
+    await this.playersService.createOrUpdatePlayer(player);
+  }
+
+  @Get()
+  async getPlayers(): Promise<Players[]> {
+    return await this.playersService.getPlayers();
+  }
+
+  @Get()
+  async findPlayer(@Query('email') email: string): Promise<Players> {
+    return await this.playersService.findPlayerByEmail(email);
+  }
+
+  @Delete()
+  async deletePlayer(@Query('email') email: string): Promise<void> {
+    await this.playersService.deletePlayer(email);
+  }
+}
