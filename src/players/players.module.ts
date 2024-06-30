b4a8cddd21186e3c4a71e89f393f834a69c188a3
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-30 00:10:09 -0300

    refactor: add database module

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
+import { DatabaseModule } from '../shared/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
