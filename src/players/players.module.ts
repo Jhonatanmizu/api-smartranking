cd42128b2a00257b1f94a410da23a46127e8bd19
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-30 00:47:40 -0300

    feat: add player providers

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
 import { DatabaseModule } from '../shared/database/database.module';
+import { PlayerProviders } from './players.provider';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [PlayersController],
-  providers: [PlayersService],
+  providers: [PlayersService, ...PlayerProviders],
 })
 export class PlayersModule {}
