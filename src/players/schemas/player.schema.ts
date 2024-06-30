64a294205ce2bea96ddf02f381b41841396d2d71
jhonatanmizu <natanjesuss20@gmail.com>
2024-06-30 00:47:20 -0300

    feat: create player schema

--- src/players/schemas/player.schema.ts
+++ src/players/schemas/player.schema.ts
@@ -1,7 +1,13 @@
 import * as mongoose from 'mongoose';
 
-export const PlayerSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  breed: String,
-});
+export const PlayerSchema = new mongoose.Schema(
+  {
+    name: { type: 'String' },
+    ranking: { type: 'String' },
+    imageUrl: { type: 'String' },
+    rankingPosition: { type: 'Number' },
+    email: { type: 'String', unique: true },
+    phoneNumber: { type: 'String', unique: true },
+  },
+  { timestamps: true, collection: 'players' },
+);
