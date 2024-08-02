a1cd7b7be01cacc62d8bb149da4a9d7818248758
jhonatanmizu <natanjesuss20@gmail.com>
2024-08-02 00:40:06 -0300

    :sparkles: feat: create providers logic

--- src/players/players.provider.ts
+++ src/players/players.provider.ts
@@ -1,11 +1,14 @@
 import { Connection } from 'mongoose';
+import { DATABASE_CONNECTION } from 'src/shared/database/database.providers';
 import { PlayerSchema } from './schemas/player.schema';
+
+export const PLAYER_MODEL = 'PLAYER_MODEL';
 
 export const PlayerProviders = [
   {
-    provide: 'PLAYER_MODEL',
+    provide: PLAYER_MODEL,
     useFactory: (connection: Connection) =>
       connection.model('Player', PlayerSchema),
-    inject: ['DATABASE_CONNECTION'],
+    inject: [DATABASE_CONNECTION],
   },
 ];
